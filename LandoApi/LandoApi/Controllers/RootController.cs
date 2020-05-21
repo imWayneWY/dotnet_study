1f244c0f7e2447232328ac17bfb5f27880a44604
weiyan <im.wayne.wy@gmail.com>
2020-05-21 10:54:17 -0600

    add swagger

---
+++ LandoApi/LandoApi/Controllers/RootController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LandoApi.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class RootController : ControllerBase
+    {
+        [HttpGet(Name = nameof(GetRoot))]
+        [ProducesResponseType(200)]
+        public IActionResult GetRoot()
+        {
+            var response = new
+            {
+                href = Url.Link(nameof(GetRoot), null),
+                rooms = new
+                {
+                    href = Url.Link(nameof(RoomController.GetRooms), null)
+                }
+            };
+            return Ok(response);
+        }
+    }
+}
--- LandoApi/LandoApi/Startup.cs
+++ LandoApi/LandoApi/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using NSwag.AspNetCore;
 
 namespace LandoApi
 {
@@ -26,6 +27,8 @@ namespace LandoApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddRouting(options => options.LowercaseUrls = true);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +37,11 @@ namespace LandoApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwaggerUi3WithApiExplorer(options =>
+                {
+                    options.GeneratorSettings.DefaultPropertyNameHandling
+                        = NJsonSchema.PropertyNameHandling.CamelCase;
+                });
             }
             else
             {
